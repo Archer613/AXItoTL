c69c91e9218fde7451c7fc483598d4537370d0c1
Cap-jinlonG <2495662239@qq.com>
2023-09-25 17:27:32 +0800

    fix respChosen

--- src/test/scala/axi2tl/AXItoTLTest.scala
+++ src/test/scala/axi2tl/AXItoTLTest.scala
@@ -16,91 +16,6 @@ import chisel3.util.DecoupledIO
 import freechips.rocketchip.amba.axi4.{AXI4BundleAR, AXI4BundleAW, AXI4BundleB, AXI4BundleParameters, AXI4BundleR, AXI4BundleW}
 import freechips.rocketchip.tilelink.{TLBundleA, TLBundleD, TLBundleParameters}
 
-class outmodule()(implicit p : Parameters) extends LazyModule{
-
-    val node = MyAXI4ToTLNode(wcorrupt = false,5)
-    lazy val module = new LazyModuleImp(this){
-    val params = p.alterPartial{
-      case EdgeInKey => node.in.head._2
-      case EdgeOutKey => node.out.head._2
-    }
-        val AXItoTL1 = Module(new  AXItoTL1(
-            AXItoTLEdgeIn = node.in.head._2,
-            AXItoTLEdgeOut = node.out.head._2,
-            AXItoTLBundleIn = node.in.head._1,
-            AXItoTLBundleOut = node.out.head._1
-        )(params))
-        AXItoTL1.io.in <> node.in.head._1
-        AXItoTL1.io.out <> node.out.head._1
-    }
-
-}
-
-class  TestDMA1()(implicit p: Parameters) extends LazyModule{
- 
-  /*   DMA
-   *    |
-   *   MEM
-   */
-  val delayFactor = 0.5
-
-  val idBits = 14
-  val device = new SimpleDevice("my-device", Seq("tutorial,my-device0"))
-  val l3FrontendAXI4Node = AXI4MasterNode(Seq(AXI4MasterPortParameters(
-    Seq(AXI4MasterParameters(
-      name = "dma",
-      id = IdRange(0, 1 << idBits),
-      maxFlight = Some(16)
-    ))
-  )))
-
-  // val xbar = TLXbar()
-  val tlnode = TLManagerNode(Seq(TLSlavePortParameters.v1(Seq(TLManagerParameters(
-    address = Seq(AddressSet(
-      0x00000000L, 0xfffffffffL
-    )),
-    resources = device.reg,
-    supportsGet = TransferSizes(1, 32),
-    supportsPutFull = TransferSizes(1, 32),
-    supportsPutPartial = TransferSizes(1, 32)
-    ,
-    fifoId = Some(0))), 8)))
- 
-
-  val axi2tlParams = p(AXI2TLParamKey)
-  val AXItoTL = LazyModule(new outmodule)
-    // TLFIFOFixer() :=
-    
-    tlnode :=
-    AXItoTL.node :=
-    l3FrontendAXI4Node
-
-    val master_nodes = l3FrontendAXI4Node
-    lazy val module = new LazyModuleImp(this){
-      master_nodes.makeIOs()(ValName(s"master_port_"))
-      tlnode.makeIOs()(ValName(s"l3_port_"))
-    }
-
-}
-
-
-object TestDMA1 extends App {
-  val config = new Config((_, _, _) => {
-    case AXI2TLParamKey => AXI2TLParam()
-  })
-  val top = DisableMonitors(p => LazyModule(new TestDMA1()(p)) )(config)
-
-  (new ChiselStage).execute(args, Seq(
-    ChiselGeneratorAnnotation(() => top.module)
-  ))
-
-  ChiselDB.init(false)
-  ChiselDB.addToFileRegisters
-  FileRegisters.write("./build")
-
-}
-
-
 
 class TestDMA()(implicit p: Parameters) extends LazyModule {
 
@@ -188,8 +103,8 @@ class TestDMA()(implicit p: Parameters) extends LazyModule {
 
   val master_nodes = l3FrontendAXI4Node
   lazy val module = new LazyModuleImp(this){
-    master_nodes.makeIOs()(ValName(s"master_port_"))
-    tlnode.makeIOs()(ValName(s"l3_port_"))
+    master_nodes.makeIOs()(ValName(s"master_port"))
+    tlnode.makeIOs()(ValName(s"l3_port"))
   }
 
 }
