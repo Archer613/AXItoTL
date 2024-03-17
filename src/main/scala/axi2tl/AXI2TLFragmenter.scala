c20339d22065ea1d6abba435fc02cefd59cb691e
Cap-jinlonG <2495662239@qq.com>
2024-03-17 16:54:22 +0800

    suporrt write size=6

--- src/main/scala/axi2tl/AXI2TLFragmenter.scala
+++ src/main/scala/axi2tl/AXI2TLFragmenter.scala
@@ -138,13 +138,91 @@ class AXI2TLFragmenter()(implicit p: Parameters) extends LazyModule
         (out, last, beats)
       }
 
+       def fragmentWrite(a: IrrevocableIO[AXI4BundleA], supportedSizes1: Seq[Int]): (IrrevocableIO[AXI4BundleA], Bool, UInt) = {
+        val out = Wire(chiselTypeOf(a))
+
+        val busy   = RegInit(false.B)
+        val r_addr = Reg(UInt(a.bits.params.addrBits.W))
+        val r_len  = Reg(UInt(AXI4Parameters.lenBits.W))
+
+        val len  = Mux(busy, r_len,  a.bits.len)
+        val addr = Mux(busy, r_addr, a.bits.addr)
+
+        val lo = if (lgBytes == 0) 0.U else addr(lgBytes-1, 0)
+        val cutoff = AXI4Parameters.lenBits + lgBytes
+        val alignment = addr((a.bits.params.addrBits min cutoff)-1, lgBytes)
+
+        // We don't care about illegal addresses; bursts or no bursts... whatever circuit is simpler (AXI4ToTL will fix it)
+        // !!! think about this more -- what if illegal?
+        val sizes1 = (supportedSizes1 zip slave.slaves.map(_.address)).filter(_._1 >= 0).groupBy(_._1).mapValues(_.flatMap(_._2))
+        val reductionMask = AddressDecoder(sizes1.values.toList)
+        val support1 = Mux1H(sizes1.toList.map { case (v, a) => // maximum supported size-1 based on target address
+          (AddressSet.unify(a.map(_.widen(~reductionMask)).distinct).map(_.contains(addr)).reduce(_||_), v.U)
+        })
+
+        /* We need to compute the largest transfer allowed by the AXI len.
+         * len+1 is the number of beats to execute.
+         * We want the MSB(len+1)-1; one less than the largest power of two we could execute.
+         * There are two cases; either len is 2^n-1 in which case we leave it unchanged, ELSE
+         *   fill the bits from highest to lowest, and shift right by one bit.
+         */
+        val fillLow  = rightOR(len) >> 1   // set   all bits in positions <  a set     bit
+        val wipeHigh = ~(leftOR(~len))       // clear all bits in position  >= a cleared bit
+        val remain1  = fillLow | wipeHigh  // MSB(a.len+1)-1
+        val align1   = ~leftOR(alignment)  // transfer size limited by address alignment
+        //!!!
+        val maxSupported1 = remain1 & align1 & support1  // Take the minimum of all the limits
+
+        // Things that cause us to degenerate to a single beat
+        val fixed = a.bits.burst === AXI4Parameters.BURST_FIXED
+        val narrow = a.bits.size =/= lgBytes.U
+        println(s" lgBytes: ${lgBytes}")
+
+        val bad = fixed || narrow
+
+        // The number of beats-1 to execute
+        val beats1 = Mux(bad, 0.U, maxSupported1)
+        val beats = OH1ToOH(beats1) // beats1 + 1
+
+        val inc_addr = addr + (beats << a.bits.size) // address after adding transfer
+        val wrapMask = a.bits.bytes1() // only these bits may change, if wrapping
+        val mux_addr = WireDefault(inc_addr)
+        when (a.bits.burst === AXI4Parameters.BURST_WRAP) {
+          mux_addr := (inc_addr & wrapMask) | ~(~a.bits.addr | wrapMask)
+        }
+        when (a.bits.burst === AXI4Parameters.BURST_FIXED) {
+          mux_addr := a.bits.addr
+        }
+
+        val last = beats1 === len
+        a.ready := out.ready && last
+        out.valid := a.valid
+
+        out.bits :<= a.bits
+        out.bits.len := beats1
+
+        // We forcibly align every access. If the first beat was misaligned, the strb bits
+        // for the lower addresses must not have been set. Therefore, rounding the address
+        // down is harmless. We can do this after the address update algorithm, because the
+        // incremented values will be rounded down the same way. Furthermore, a subword
+        // offset cannot cause a premature wrap-around.
+        out.bits.addr := ~(~addr | UIntToOH1(a.bits.size, lgBytes))
+
+        when (out.fire) {
+          busy := !last
+          r_addr := mux_addr
+          r_len  := len - beats
+        }
+
+        (out, last, beats)
+      }
       // The size to which we will fragment the access
       val readSizes1  = slaves.map(s => s.supportsRead .max/beatBytes-1)
       val writeSizes1 = slaves.map(s => s.supportsWrite.max/beatBytes-1)
 
       // Irrevocable queues in front because we want to accept the request before responses come back
       val (in_ar, ar_last, _)       = fragment(Queue.irrevocable(in.ar, 1, flow=true), readSizes1)
-      val (in_aw, aw_last, w_beats) = fragment(Queue.irrevocable(in.aw, 1, flow=true), writeSizes1)
+      val (in_aw, aw_last, w_beats) = fragmentWrite(Queue.irrevocable(in.aw, 1, flow=true), writeSizes1)
 
       // AXI ready may not depend on valid of other channels
       // We cut wready here along with awready and arready before AXI4ToTL
--- src/main/scala/axi2tl/WriteStack.scala
+++ src/main/scala/axi2tl/WriteStack.scala
@@ -25,7 +25,7 @@ class writeEntry(implicit p: Parameters) extends AXItoTLBundle {
   val count = UInt(axiLenBits.W)
   val d_resp = UInt(2.W)
   val size = UInt(axiSizeBits.W)
-  val len = UInt(axiLenBits.W)
+  val len = UInt((1 << axiLenBits).W)
   val waitSendBRespFifoId = UInt(axi2tlParams.wbufIdBits.W)
   val real_last = Bool()
 }
@@ -120,6 +120,7 @@ class NewWriteStack(
       entry.entryid := idxInsert
       entry.real_last := io.in.aw.bits.echo(AXI4FragLast)
       entry.count := 0.U
+      
   }
     /* ======== Receive W Req ======== */
   /*
@@ -180,255 +181,258 @@ class NewWriteStack(
       }
 
 }
-class WriteStack(
-  entries: Int = 8
-)(
-  implicit p: Parameters)
-    extends AXItoTLModule
-    with HasPerfLogging {
-  val io = IO(new Bundle() {
-    val in = new Bundle() {
-      val aw = Flipped(
-        DecoupledIO(
-          new AXI4BundleAW(
-            edgeIn.bundle
-          )
-        )
-      )
-      val w = Flipped(
-        DecoupledIO(
-          new AXI4BundleW(
-            edgeIn.bundle
-          )
-        )
-      )
-      val b = DecoupledIO(
-        new AXI4BundleB(
-          edgeIn.bundle
-        )
-      )
-    }
-    val out = new Bundle() {
-      val a = DecoupledIO(new TLBundleA(edgeOut.bundle))
-      val d = Flipped(
-        DecoupledIO(
-          new TLBundleD(
-            edgeOut.bundle
-          )
-        )
-      )
-    }
-  })
+// class WriteStack(
+//   entries: Int = 8
+// )(
+//   implicit p: Parameters)
+//     extends AXItoTLModule
+//     with HasPerfLogging {
+//   val io = IO(new Bundle() {
+//     val in = new Bundle() {
+//       val aw = Flipped(
+//         DecoupledIO(
+//           new AXI4BundleAW(
+//             edgeIn.bundle
+//           )
+//         )
+//       )
+//       val w = Flipped(
+//         DecoupledIO(
+//           new AXI4BundleW(
+//             edgeIn.bundle
+//           )
+//         )
+//       )
+//       val b = DecoupledIO(
+//         new AXI4BundleB(
+//           edgeIn.bundle
+//         )
+//       )
+//     }
+//     val out = new Bundle() {
+//       val a = DecoupledIO(new TLBundleA(edgeOut.bundle))
+//       val d = Flipped(
+//         DecoupledIO(
+//           new TLBundleD(
+//             edgeOut.bundle
+//           )
+//         )
+//       )
+//     }
+//   })
 
-  /*
-     Data Structure:
-       readStack: store  contrl imformation
-       readDataStack : store r_data
-   */
+//   /*
+//      Data Structure:
+//        readStack: store  contrl imformation
+//        readDataStack : store r_data
+//    */
 
-  val writeStack = RegInit(VecInit(Seq.fill(entries)(0.U.asTypeOf(new writeEntry))))
-  val writeDataStack = Module(
-    new SRAMTemplate(
-      gen = new WSBlock,
-      set = entries,
-      way = 1,
-      singlePort = true,
-      hasMbist = p(AXI2TLParamKey).hasMbist,
-      hasShareBus = p(AXI2TLParamKey).hasShareBus,
-      parentName = "axi2tl_write_"
-    )
-  )
-  val mbistPipeline =
-    MBISTPipeline.PlaceMbistPipeline(1, s"MBIST_AXI2TL_W_", p(AXI2TLParamKey).hasMbist && p(AXI2TLParamKey).hasShareBus)
-  val idel :: waitW :: sendPut :: waitDResp :: sendB :: Nil = Enum(5)
-  val wreqArb = Module(new Arbiter(new writeEntry, entries))
-  val sendBArb = Module(new Arbiter(new writeEntry, entries))
+//   val writeStack = RegInit(VecInit(Seq.fill(entries)(0.U.asTypeOf(new writeEntry))))
+//   val writeDataStack = Module(
+//     new SRAMTemplate(
+//       gen = new WSBlock,
+//       set = entries,
+//       way = 1,
+//       singlePort = true,
+//       hasMbist = p(AXI2TLParamKey).hasMbist,
+//       hasShareBus = p(AXI2TLParamKey).hasShareBus,
+//       parentName = "axi2tl_write_"
+//     )
+//   )
+//   val mbistPipeline =
+//     MBISTPipeline.PlaceMbistPipeline(1, s"MBIST_AXI2TL_W_", p(AXI2TLParamKey).hasMbist && p(AXI2TLParamKey).hasShareBus)
+//   val idel :: waitW :: sendPut :: waitDResp :: sendB :: Nil = Enum(5)
+//   val wreqArb = Module(new Arbiter(new writeEntry, entries))
+//   val sendBArb = Module(new Arbiter(new writeEntry, entries))
 
-  /*
-    when aw is fire and readStack is not full,
-    alloc a entry in readStack and readDataStack.
-   */
-  val full = Cat(writeStack.map(_.wvalid)).andR
-  val hasWaitW = Cat(writeStack.map(e => e.wvalid && e.wstatus === waitW)).orR
-  val alloc = !full && !hasWaitW
-  val idxInsert = Mux(alloc, PriorityEncoder(writeStack.map(!_.wvalid)), 0.U)
+//   /*
+//     when aw is fire and readStack is not full,
+//     alloc a entry in readStack and readDataStack.
+//    */
+//   val full = Cat(writeStack.map(_.wvalid)).andR
+//   val hasWaitW = Cat(writeStack.map(e => e.wvalid && e.wstatus === waitW)).orR
+//   val alloc = !full && !hasWaitW
+//   val idxInsert = Mux(alloc, PriorityEncoder(writeStack.map(!_.wvalid)), 0.U)
   
-  //when write stack is not full,can receive aw request
-  io.in.aw.ready := !full && !hasWaitW
-  when(alloc && io.in.aw.fire) {
-    val entry = writeStack(idxInsert)
-    entry.wvalid := true.B
-    entry.waddr := io.in.aw.bits.addr
-    entry.wstatus := waitW
-    entry.wsize := OH1ToUInt(io.in.aw.bits.bytes1().asUInt)
-    entry.size := io.in.aw.bits.size
-    entry.len := io.in.aw.bits.len
-    entry.awid := io.in.aw.bits.id
-    entry.entryid := idxInsert
-    entry.real_last := io.in.aw.bits.echo(AXI4FragLast)
-  }
+//   //when write stack is not full,can receive aw request
+//   io.in.aw.ready := !full && !hasWaitW
+//   when(alloc && io.in.aw.fire) {
+//     val entry = writeStack(idxInsert)
+//     entry.wvalid := true.B
+//     entry.waddr := io.in.aw.bits.addr
+//     entry.wstatus := waitW
+//     entry.wsize := OH1ToUInt(io.in.aw.bits.bytes1().asUInt)
+//     entry.size := io.in.aw.bits.size
+//     entry.len := io.in.aw.bits.len
+//     entry.awid := io.in.aw.bits.id
+//     entry.entryid := idxInsert
+//     entry.real_last := io.in.aw.bits.echo(AXI4FragLast)
+//   }
 
-  /* ======== Receive W Req ======== */
-  /*
-        Since the W channel in AXI4 does not use wid,
-        each time an AW task is accepted, it must wait for a w task.
-        data and mask will be stored in Sram
-   */
-  val wFire = io.in.w.fire
-  val canW = Cat(writeStack.map(e => e.wvalid && e.wstatus === waitW && RegNext(!wFire,false.B) && e.waitWFifoId === 0.U)).orR
-  val wen = io.in.w.fire
-  val wsbIdx =dontTouch(WireInit(0.U))
-  io.in.w.ready := canW
-  writeStack.foreach { e =>
-    when(e.wvalid && e.wstatus === waitW && e.waitWFifoId === 0.U) {
-      wsbIdx := e.entryid
-    }
-  }
-  writeDataStack.io.w.apply(wen, WSBlock(io.in.w.bits.data, io.in.w.bits.strb), wsbIdx, 1.U)
+//   /* ======== Receive W Req ======== */
+//   /*
+//         Since the W channel in AXI4 does not use wid,
+//         each time an AW task is accepted, it must wait for a w task.
+//         data and mask will be stored in Sram
+//    */
+//   val wFire = io.in.w.fire
+//   val canW = Cat(writeStack.map(e => e.wvalid && e.wstatus === waitW && RegNext(!wFire,false.B) && e.waitWFifoId === 0.U)).orR
+//   val wen = io.in.w.fire
+//   val wsbIdx =dontTouch(WireInit(0.U))
+//   io.in.w.ready := canW
+//   writeStack.foreach { e =>
+//     when(e.wvalid && e.wstatus === waitW && e.waitWFifoId === 0.U) {
+//       wsbIdx := e.entryid
+//     }
+//   }
+//   writeDataStack.io.w.apply(wen, WSBlock(io.in.w.bits.data, io.in.w.bits.strb), wsbIdx, 1.U)
   
 
-  /* ======== Send ======== */
-  //when a entry is fire,send a put request to L3
+//   /* ======== Send ======== */
+//   //when a entry is fire,send a put request to L3
 
-  wreqArb.io.in.zip(writeStack).foreach {
-    case (in, e) =>
-      in.valid := e.wvalid && e.wstatus === sendPut && e.entryFifoid === 0.U && e.wready
-      in.bits := e
-  }
-  val WSBIdx1 = dontTouch(wreqArb.io.chosen)
-  val ren = dontTouch(wreqArb.io.out.valid && !wen)
-  // val rdDataRaw = RegNextN(array.io.r.resp.data(0), sramLatency - 1) // DSBlock
-  val write_data = writeDataStack.io.r.resp.data(0).data
-  val write_mask = writeDataStack.io.r.resp.data(0).mask
-  val write_size = RegNextN(wreqArb.io.out.bits.wsize, sramLatency - 1)
-  val mask_bits = dontTouch(PopCount(write_mask))
-  val write_bits = dontTouch(1.U << io.out.a.bits.size)
-  // assert((mask_bits <= write_bits && io.out.a.fire) || !io.out.a.fire,"AXItoTL: write bits is too much")
-  writeDataStack.io.r.apply(ren, wreqArb.io.chosen)
-  io.out.a.valid := RegNextN(ren, sramLatency - 1)
-  io.out.a.bits.opcode := RegNextN(TLMessages.PutPartialData, sramLatency - 1)
-  io.out.a.bits.param := 0.U
-  io.out.a.bits.size := RegNextN(wreqArb.io.out.bits.wsize, sramLatency - 1)
-  io.out.a.bits.source := RegNextN(
-    Cat(0.asUInt, wreqArb.io.out.bits.entryid, wreqArb.io.out.bits.awid),
-    sramLatency - 1
-  )
-  io.out.a.bits.address := RegNextN(wreqArb.io.out.bits.waddr, sramLatency - 1)
-  io.out.a.bits.mask := writeDataStack.io.r.resp.data(0).mask
-  io.out.a.bits.data := writeDataStack.io.r.resp.data(0).data
-  io.out.a.bits.corrupt := false.B
-  wreqArb.io.out.ready := io.out.a.ready
+//   wreqArb.io.in.zip(writeStack).foreach {
+//     case (in, e) =>
+//       in.valid := e.wvalid && e.wstatus === sendPut && e.entryFifoid === 0.U && e.wready
+//       in.bits := e
+//   }
+//   val WSBIdx1 = dontTouch(wreqArb.io.chosen)
+//   val ren = dontTouch(wreqArb.io.out.valid && !wen)
+//   // val rdDataRaw = RegNextN(array.io.r.resp.data(0), sramLatency - 1) // DSBlock
+//   val write_data = writeDataStack.io.r.resp.data(0).data
+//   val write_mask = writeDataStack.io.r.resp.data(0).mask
+//   val write_size = RegNextN(wreqArb.io.out.bits.wsize, sramLatency - 1)
+//   val mask_bits = dontTouch(PopCount(write_mask))
+//   val write_bits = dontTouch(1.U << io.out.a.bits.size)
+//   // assert((mask_bits <= write_bits && io.out.a.fire) || !io.out.a.fire,"AXItoTL: write bits is too much")
+//   writeDataStack.io.r.apply(ren, wreqArb.io.chosen)
+//   io.out.a.valid := RegNextN(ren, sramLatency - 1)
+//   io.out.a.bits.opcode := RegNextN(TLMessages.PutPartialData, sramLatency - 1)
+//   io.out.a.bits.param := 0.U
+//   io.out.a.bits.size := RegNextN(wreqArb.io.out.bits.wsize, sramLatency - 1)
+//   io.out.a.bits.source := RegNextN(
+//     Cat(0.asUInt, wreqArb.io.out.bits.entryid, wreqArb.io.out.bits.awid),
+//     sramLatency - 1
+//   )
+//   io.out.a.bits.address := RegNextN(wreqArb.io.out.bits.waddr, sramLatency - 1)
+//   io.out.a.bits.mask := writeDataStack.io.r.resp.data(0).mask
+//   io.out.a.bits.data := writeDataStack.io.r.resp.data(0).data
+//   io.out.a.bits.corrupt := false.B
+//   wreqArb.io.out.ready := io.out.a.ready
 
-  /* ======== Receive d resp and Send b resp ======== */
-  val canRecD = Cat(writeStack.map(e => e.wvalid && e.wstatus === waitDResp)).orR
-  val d_valid = io.out.d.fire
-  io.out.d.ready := canRecD
+//   /* ======== Receive d resp and Send b resp ======== */
+//   val canRecD = Cat(writeStack.map(e => e.wvalid && e.wstatus === waitDResp)).orR
+//   val d_valid = io.out.d.fire
+//   io.out.d.ready := canRecD
 
-  when(d_valid) {
-    val sourceD = io.out.d.bits.source
-    val wsIdx = sourceD(axiIdBits + axi2tlParams.wbufIdBits - 1, axiIdBits).asUInt
-    // val wsIdx = sourceD(axi2tlParams.wbufIdBits - 1,0).asUInt
-    writeStack(wsIdx).wstatus := sendB
-    writeStack(wsIdx).d_resp := Mux(
-      io.out.d.bits.denied || io.out.d.bits.corrupt,
-      AXI4Parameters.RESP_SLVERR,
-      AXI4Parameters.RESP_OKAY
-    )
-  }
+//   when(d_valid) {
+//     val sourceD = io.out.d.bits.source
+//     val wsIdx = sourceD(axiIdBits + axi2tlParams.wbufIdBits - 1, axiIdBits).asUInt
+//     // val wsIdx = sourceD(axi2tlParams.wbufIdBits - 1,0).asUInt
+//     writeStack(wsIdx).wstatus := sendB
+//     writeStack(wsIdx).d_resp := Mux(
+//       io.out.d.bits.denied || io.out.d.bits.corrupt,
+//       AXI4Parameters.RESP_SLVERR,
+//       AXI4Parameters.RESP_OKAY
+//     )
+//   }
 
-  when(d_valid && io.in.b.fire) {
-    val sourceD = io.out.d.bits.source
-    val wsIdx = sourceD(axiIdBits + axi2tlParams.wbufIdBits - 1, axiIdBits).asUInt
+//   when(d_valid && io.in.b.fire) {
+//     val sourceD = io.out.d.bits.source
+//     val wsIdx = sourceD(axiIdBits + axi2tlParams.wbufIdBits - 1, axiIdBits).asUInt
 
-    // val wsIdx = sourceD( axi2tlParams.wbufIdBits - 1, 0).asUInt
-    writeStack(wsIdx).waitSendBRespFifoId := PopCount(writeStack.map(e => e.wvalid && e.wstatus === sendB)) - 1.U
-  }.elsewhen(d_valid && !io.in.b.fire) {
-    val sourceD = io.out.d.bits.source
-    val wsIdx = sourceD(axiIdBits + axi2tlParams.wbufIdBits - 1, axiIdBits).asUInt
-    // val wsIdx = sourceD( axi2tlParams.wbufIdBits - 1, 0).asUInt
+//     // val wsIdx = sourceD( axi2tlParams.wbufIdBits - 1, 0).asUInt
+//     writeStack(wsIdx).waitSendBRespFifoId := PopCount(writeStack.map(e => e.wvalid && e.wstatus === sendB)) - 1.U
+//   }.elsewhen(d_valid && !io.in.b.fire) {
+//     val sourceD = io.out.d.bits.source
+//     val wsIdx = sourceD(axiIdBits + axi2tlParams.wbufIdBits - 1, axiIdBits).asUInt
+//     // val wsIdx = sourceD( axi2tlParams.wbufIdBits - 1, 0).asUInt
 
-    writeStack(wsIdx).waitSendBRespFifoId := PopCount(writeStack.map(e => e.wvalid && e.wstatus === sendB))
-  }
-  sendBArb.io.in.zip(writeStack).foreach {
-    case (in, e) =>
-      in.valid := e.wvalid && e.wstatus === sendB && e.waitSendBRespFifoId === 0.U
-      in.bits := e
-  }
+//     writeStack(wsIdx).waitSendBRespFifoId := PopCount(writeStack.map(e => e.wvalid && e.wstatus === sendB))
+//   }
+//   sendBArb.io.in.zip(writeStack).foreach {
+//     case (in, e) =>
+//       in.valid := e.wvalid && e.wstatus === sendB && e.waitSendBRespFifoId === 0.U
+//       in.bits := e
+//   }
 
-  val chosenBIdx = sendBArb.io.chosen
-  sendBArb.io.out.ready := io.in.b.ready
-  io.in.b.valid := sendBArb.io.out.valid
-  io.in.b.bits.id := writeStack(chosenBIdx).awid
-  io.in.b.bits.resp := writeStack(chosenBIdx).d_resp
-  io.in.b.bits.echo(AXI4FragLast) := writeStack(chosenBIdx).real_last
+//   val chosenBIdx = sendBArb.io.chosen
+//   sendBArb.io.out.ready := io.in.b.ready
+//   io.in.b.valid := sendBArb.io.out.valid
+//   io.in.b.bits.id := writeStack(chosenBIdx).awid
+//   io.in.b.bits.resp := writeStack(chosenBIdx).d_resp
+//   io.in.b.bits.echo(AXI4FragLast) := writeStack(chosenBIdx).real_last
 
-  /* ======== Update wstatus and Fifoid ======== */
+//   /* ======== Update wstatus and Fifoid ======== */
 
-  val datawillready = RegNext(io.in.w.fire, false.B)
-  when(io.in.aw.fire && datawillready) {
-    val entry = writeStack(idxInsert)
-    entry.waitWFifoId := PopCount(writeStack.map(e => e.wvalid && (e.wstatus === waitW))) - 1.U
-  }.elsewhen(io.in.aw.fire && !datawillready) {
-    val entry = writeStack(idxInsert)
-    entry.waitWFifoId := PopCount(writeStack.map(e => e.wvalid && (e.wstatus === waitW)))
-  }
+//   val datawillready = RegNext(io.in.w.fire, false.B)
+//   when(io.in.aw.fire && datawillready) {
+//     val entry = writeStack(idxInsert)
+//     entry.waitWFifoId := PopCount(writeStack.map(e => e.wvalid && (e.wstatus === waitW))) - 1.U
+//   }.elsewhen(io.in.aw.fire && !datawillready) {
+//     val entry = writeStack(idxInsert)
+//     entry.waitWFifoId := PopCount(writeStack.map(e => e.wvalid && (e.wstatus === waitW)))
+//   }
 
-  when(datawillready) {
-    writeStack.foreach { e =>
-      when(e.wvalid && e.wstatus === waitW && e.waitWFifoId === 0.U) {
-        e.wstatus := sendPut
-        e.waitWFifoId := e.waitWFifoId - 1.U
-        e.wready := true.B
-      }.elsewhen(e.wvalid && (e.wstatus === waitW)) {
-        e.waitWFifoId := e.waitWFifoId - 1.U
-      }
-    }
-  }
+//   when(datawillready) {
+//     writeStack.foreach { e =>
+//       when(e.wvalid && e.wstatus === waitW && e.waitWFifoId === 0.U) {
+//         e.wstatus := sendPut
+//         e.waitWFifoId := e.waitWFifoId - 1.U
+//         e.wready := true.B
+//       }.elsewhen(e.wvalid && (e.wstatus === waitW)) {
+//         e.waitWFifoId := e.waitWFifoId - 1.U
+//       }
+//     }
+//   }
 
-  val willput = RegNextN(wreqArb.io.out.valid && !io.in.w.fire && io.out.a.ready, sramLatency - 1)
-  when(wreqArb.io.out.valid && !io.in.w.fire && io.out.a.ready) {
-    writeStack(WSBIdx1).wready := false.B
-  }
-  when(willput) {
-    writeStack.foreach { e =>
-      when(e.wvalid && e.wstatus === sendPut && e.entryFifoid === 0.U) {
-        e.wstatus := waitDResp
-      }
-      when(e.wvalid) {
-        e.entryFifoid := e.entryFifoid - 1.U
-      }
-    }
-  }
+//   val willput = RegNextN(wreqArb.io.out.valid && !io.in.w.fire && io.out.a.ready, sramLatency - 1)
+//   when(wreqArb.io.out.valid && !io.in.w.fire && io.out.a.ready) {
+//     writeStack(WSBIdx1).wready := false.B
+//   }
+//   when(willput) {
+//     writeStack.foreach { e =>
+//       when(e.wvalid && e.wstatus === sendPut && e.entryFifoid === 0.U) {
+//         e.wstatus := waitDResp
+//       }
+//       when(e.wvalid) {
+//         e.entryFifoid := e.entryFifoid - 1.U
+//       }
+//     }
+//   }
 
-  when(io.in.aw.fire && willput) {
-    val entry = writeStack(idxInsert)
-    entry.entryFifoid := PopCount(writeStack.map(e => e.wvalid && (e.wstatus === waitW || e.wstatus === sendPut))) - 1.U
-  }.elsewhen(io.in.aw.fire && !willput) {
-    val entry = writeStack(idxInsert)
-    entry.entryFifoid := PopCount(writeStack.map(e => e.wvalid && (e.wstatus === waitW || e.wstatus === sendPut)))
-  }
+//   when(io.in.aw.fire && willput) {
+//     val entry = writeStack(idxInsert)
+//     entry.entryFifoid := PopCount(writeStack.map(e => e.wvalid && (e.wstatus === waitW || e.wstatus === sendPut))) - 1.U
+//   }.elsewhen(io.in.aw.fire && !willput) {
+//     val entry = writeStack(idxInsert)
+//     entry.entryFifoid := PopCount(writeStack.map(e => e.wvalid && (e.wstatus === waitW || e.wstatus === sendPut)))
+//   }
 
-  when(io.in.b.fire) {
-    writeStack.foreach { e =>
-      when(e.wvalid && e.wstatus === sendB && e.waitSendBRespFifoId === 0.U) {
-        e.wvalid := false.B
-        e.wstatus := idel
-        e.waitSendBRespFifoId := (entries - 1).U
-        //                  e.wready := false.B
-      }
-      when(e.wvalid && e.wstatus === sendB) {
-        e.waitSendBRespFifoId := e.waitSendBRespFifoId - 1.U
-      }
-    }
-  }
-  if (axi2tlParams.enablePerf) {
-    XSPerfAccumulate("writeStack_full", full)
-    XSPerfAccumulate("writeStack_alloc", alloc)
-    XSPerfAccumulate("writeStack_recv_aw_req", io.in.aw.fire)
-    XSPerfAccumulate("writeStack_recv_w_req", io.in.w.fire)
-    XSPerfAccumulate("writeStack_send_a_req", io.out.a.fire)
-    XSPerfAccumulate("writeStack_recv_d_resp", io.out.d.fire)
-    XSPerfAccumulate("writeStack_send_b_resp", io.in.b.fire)
-  }
+//   when(io.in.b.fire) {
+//     writeStack.foreach { e =>
+//       when(e.wvalid && e.wstatus === sendB && e.waitSendBRespFifoId === 0.U ) {
+//         e.wvalid := false.B
+//         e.wstatus := idel
+//         e.waitSendBRespFifoId := (entries - 1).U
+//         //                  e.wready := false.B
+//       }
+//       // when(e.wvalid && e.wstatus === sendB && e.waitSendBRespFifoId === 0.U && e.count < e.len) {
+//       //   e.count := e.count + 1.U
+//       // }
+//       when(e.wvalid && e.wstatus === sendB ) {
+//         e.waitSendBRespFifoId := e.waitSendBRespFifoId - 1.U
+//       }
+//     }
+//   }
+//   if (axi2tlParams.enablePerf) {
+//     XSPerfAccumulate("writeStack_full", full)
+//     XSPerfAccumulate("writeStack_alloc", alloc)
+//     XSPerfAccumulate("writeStack_recv_aw_req", io.in.aw.fire)
+//     XSPerfAccumulate("writeStack_recv_w_req", io.in.w.fire)
+//     XSPerfAccumulate("writeStack_send_a_req", io.out.a.fire)
+//     XSPerfAccumulate("writeStack_recv_d_resp", io.out.d.fire)
+//     XSPerfAccumulate("writeStack_send_b_resp", io.in.b.fire)
+//   }
 
-}
+// }
